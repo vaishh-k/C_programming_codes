f99cabb04fa16a306bd786f3fbdbb30c8ff275a1
ShraddhaShiledar <shraaddha007@gmail.com>
2026-09-12 22:08:32 +0530

    executed sucessfully

---
+++ circle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float pi = 3.14;
+    int r = 6;
+
+    //area of circle
+    float a = pi*r*r;
+    float p = 2*pi*r;
+
+    printf("Area of circle is %f \n" ,a,"inches");
+    printf("perimeter of circle is%f\n ", p,"inches");
+    return 0;
+}
