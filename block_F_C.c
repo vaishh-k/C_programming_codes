f4b5b6ccfb7320361bf2ee85e07b2944c934f358
vaishnavi621-code <vaishnavikhandagale621@gmail.com>
2026-09-13 21:51:21 +0530

    executed successfully

---
+++ block_F_C.c
@@ -0,0 +1,21 @@
+/* Write a C program to print a block F using the hash (#), 
+where the F has a height of six characters and width of five and four characters. 
+And also print a very large 'C'.*/
+
+#include<stdio.h>
+int main(){
+    printf("######\n");
+    printf("#\n");
+    printf("#\n");
+    printf("####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("\n");
+    printf("####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("####\n");
+
+
+}
