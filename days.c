639d9b367c26fe7db88cf6c98d165615750f5320
ShraddhaShiledar <shraaddha007@gmail.com>
2026-09-13 23:36:47 +0530

    Added days program

---
+++ days.c
@@ -0,0 +1,21 @@
+//* Write a C program to convert specified days into years, weeks and days.
+#include <stdio.h>
+
+int main()
+{
+    int days, years, weeks;
+
+    days = 1329; // Total number of days
+
+    // Converts days to years, weeks and days
+    years = days/365; // Calculate years
+    weeks = (days % 365)/7; // Calculate weeks
+    days = days - ((years*365) + (weeks*7)); // Calculate remaining days
+
+    // Print the results
+    printf("Years: %d\n", years);
+    printf("Weeks: %d\n", weeks);
+    printf("Days: %d \n", days);
+
+    return 0;
+}
